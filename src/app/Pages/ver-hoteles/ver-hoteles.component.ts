dcff70797a57200e37065060e74665896bc26a9b
jonnathanguaman <jonnathan.guaman.est@tecazuay.edu.ec>
2025-01-22 16:32:06 -0500

    img hoteles solucionado

--- src/app/Pages/ver-hoteles/ver-hoteles.component.ts
+++ src/app/Pages/ver-hoteles/ver-hoteles.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { HotelesService } from '../../Services/hoteles/hoteles.service';
 import { Hoteles } from '../../Services/hoteles/hoteles';
 import { environment } from '../../../enviroments/enviroment';
+import { ImagenesHoteles } from '../../Services/imagenesHoteles/imagesHoteles';
+import { ImagenesHotelesService } from '../../Services/imagenesHoteles/imagenes-hoteles.service';
+import { error } from 'console';
 
 @Component({
   selector: 'app-ver-hoteles',
@@ -10,7 +13,9 @@ import { environment } from '../../../enviroments/enviroment';
 })
 export class VerHotelesComponent implements OnInit{
   
-  constructor(private hotelesService:HotelesService){}
+  constructor(private hotelesService:HotelesService,
+    private imgDeHotelService:ImagenesHotelesService
+  ){}
 
   @Input() idLugar?:number
 
@@ -29,6 +34,21 @@ export class VerHotelesComponent implements OnInit{
     obtenerTodosHoteles(){
       this.hotelesService.getTodosHoteles().subscribe(hoteles =>{
         this.todoshoteles = hoteles
+
+          this.todoshoteles.forEach(hotel =>{
+            this.obtenerImagesDeHoteles(hotel.idHotel).then(img => {
+              hotel.imagenesHoteles = img
+            })
+          })
+      })
+    }
+
+    obtenerImagesDeHoteles(idHoteles:number):Promise<ImagenesHoteles[]>{
+      return new Promise((resolve,reject) =>{
+          this.imgDeHotelService.getImagenesHoteles(idHoteles).subscribe(
+            imgHoteles => resolve(imgHoteles),
+            error => reject(error)
+          )
       })
     }
 
